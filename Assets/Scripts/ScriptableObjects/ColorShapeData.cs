4b3b51670b17e2b1e3721118e2dde1bb4a5d0f57
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-10-06 23:43:05 -0500

    Scripts Set Up

---
+++ Assets/Scripts/ScriptableObjects/ColorShapeData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorShapeData", menuName = "Scriptable Objects/Game 1/ColorShapeData")]
+public class ColorShapeData : ScriptableObject
+{
+    public Color color;
+    public Sprite sprite;
+}
